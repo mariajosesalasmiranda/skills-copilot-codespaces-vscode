dad3d0ebff93b26f5b43ea591a679ba9b402f8c4
Maria José Salas Miranda <129628251+mariajosesalasmiranda@users.noreply.github.com>
2023-07-29 14:16:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,18 @@
-// Create web server 
+// Create web server
+// 1. Create a web server
+// 2. Create a route for '/' and send a response
+// 3. Create a route for '/about' and send a response
+// 4. Create a route for '/help' and send a response
+// 5. Start and test your work
+
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// Setup static directory to serve
+app.
